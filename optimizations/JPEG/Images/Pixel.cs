3d5963bfd53ba8b71bdf4b346bc0b6b0872c8db2
Maxim <dddg.dddfgg@yandex.ru>
2022-03-16 15:01:12 +0500

    add FillOppositeComponents

--- optimizations/JPEG/Images/Pixel.cs
+++ optimizations/JPEG/Images/Pixel.cs
@@ -5,41 +5,46 @@ namespace JPEG.Images
 {
     public class Pixel
     {
-        private readonly PixelFormat format;
-
-        public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
+        public Pixel(float firstComponent, float secondComponent, float thirdComponent, PixelFormat pixelFormat)
         {
             if (!new[]{PixelFormat.RGB, PixelFormat.YCbCr}.Contains(pixelFormat))
                 throw new FormatException("Unknown pixel format: " + pixelFormat);
-            format = pixelFormat;
             if (pixelFormat == PixelFormat.RGB)
             {
-                r = firstComponent;
-                g = secondComponent;
-                b = thirdComponent;
+                R = firstComponent;
+                G = secondComponent;
+                B = thirdComponent;
             }
             if (pixelFormat == PixelFormat.YCbCr)
             {
-                y = firstComponent;
-                cb = secondComponent;
-                cr = thirdComponent;
+                Y = firstComponent;
+                Cb = secondComponent;
+                Cr = thirdComponent;
             }
+
+            FillOppositeComponents(pixelFormat);
         }
 
-        private readonly double r;
-        private readonly double g;
-        private readonly double b;
+        private void FillOppositeComponents(PixelFormat pixelFormat)
+        {
+            if (pixelFormat == PixelFormat.RGB)
+            {
+                Y = 16.0f + (65.738f * R + 129.057f * G + 24.064f * B) / 256.0f;
+                Cb =  128.0f + (-37.945f * R - 74.494f * G + 112.439f * B) / 256.0f;
+                Cr = 128.0f + (112.439f * R - 94.154f * G - 18.285f * B) / 256.0f;
+                return;
+            }
+            R = (298.082f * Y + 408.583f * Cr) / 256.0f - 222.921f;
+            G =  (298.082f * Y - 100.291f * Cb - 208.120f * Cr) / 256.0f + 135.576f;
+            B = (298.082f * Y + 516.412f * Cb) / 256.0f - 276.836f;
+        }
 
-        private readonly double y;
-        private readonly double cb;
-        private readonly double cr;
+        public float R { get; private set; }
+        public float G { get; private set; }
+        public float B { get; private set; }
 
-        public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-        public double G => format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-        public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
-
-        public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-        public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-        public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        public float Y { get; private set; }
+        public float Cb { get; private set; }
+        public float Cr { get; private set; }
     }
 }
